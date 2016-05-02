86313f47302fbec7f3e826911bde4aef65f5ed67
Gabriel Jordão <gabrieljt@gmail.com>
2016-05-02 19:27:14 -0300

    Generating Instanced Static Mesh via BP

---
+++ Source/Voxels/VoxelsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "VoxelsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOXELS_API AVoxelsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
